7bf280ee653d7c7cf66ba04792ed84a1b67af9ae
rickydia <rickydia@umich.edu>
2017-04-29 23:46:07 -0700

    Updated my python notes

---
+++ hello.py
@@ -0,0 +1,11 @@
+print("Hello, World")
+
+spam = 4
+print(spam)
+
+string_1 = "Camelot"
+string_2 = "place"
+
+print("Let's not go to %s. 'Tis a silly %s." % (string_1, string_2))
+
+#to actually run the program, type "python3 hello.py"

4916a97f89f293f95fb69b47f855d00b6d09c0b1
rickydia <rickydia@umich.edu>
2018-02-27 20:30:33 -0800

    did a bit. learned how to call https shit and load shit onto app

--- reactnativePractice/albums/src/components/AlbumList.js
+++ reactnativePractice/albums/src/components/AlbumList.js
@@ -1,17 +1,30 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import axios from 'axios';
+import AlbumDetail from './AlbumDetail';
 
 // class based components used when you wanna fetch data or when you have large component that requires helper methods
 class AlbumList extends Component {
+	state = { albums: [] };
+
 	componentWillMount() {
-		console.log('did this work');
+		axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+			.then(response => this.setState({ albums: response.data }));
+	};
+
+	renderAlbums() {
+		return this.state.albums.map(album =>
+			<AlbumDetail key={album.title} album={album}/>
+		);
 	};
 
 	// only requirement of class based components is that you define a render method that returns some type of jsx
 	render() {
+		console.log(this.state);
+
 		return (
 			<View>
-				<Text>Album List</Text>
+				{this.renderAlbums()}
 			</View>
 		);
 	}

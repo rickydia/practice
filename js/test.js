80691309b0963403118e6a8a424034b88e948084
rickydia <rickydia@umich.edu>
2018-06-07 14:10:02 -0700

    added notes about scope

---
+++ js/test.js
@@ -0,0 +1,16 @@
+const starCount = () => {
+  let i = 1500;
+  console.log(i);
+  if(true){
+    let i = 10;
+    console.log(i);
+  }
+  console.log(i);
+  for(i = 0; i < 12;i++){
+    let i = 0;
+    console.log(i);
+  }
+  console.log(i);
+};
+
+starCount();

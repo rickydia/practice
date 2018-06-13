3f8e9609e95d0ed483ec4def6f4141d5936e1da8
rickydia <rickydia@umich.edu>
2018-06-13 02:15:31 -0700

    learned how to use babel and how to transpile ES6 code to ES5. tested it on test.js, which is now in ./src/

---
+++ js/lib/test.js
@@ -0,0 +1,18 @@
+"use strict";
+
+var starCount = function starCount() {
+  var i = 1500;
+  console.log(i);
+  if (true) {
+    var _i = 10;
+    console.log(_i);
+  }
+  console.log(i);
+  for (i = 0; i < 12; i++) {
+    var _i2 = 0;
+    console.log(_i2);
+  }
+  console.log(i);
+};
+
+starCount();
